7108f9d0ca745bca6c0cd64e70ddd681c27b95cd
rkgmf1230 <rkgmf1230@naver.com>
2024-03-28 14:01:41 +0900

    first-commit

---
+++ pages/about.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-page padding>
+    <div class="q-my-xl text-center">
+      <div class="text-h4">About</div>
+      <p class="q-mt-sm text-grey-8">vue & Nuxt Mastery Class 웹은 Nuxt3로 만들어졌습니다.</p>
+      <div class="column">
+        <RouterLink to="/">RouterLink Home</RouterLink>
+        <RouterLink to="https://youtube.com/@gymcoding">RouterLink youtube</RouterLink>
+        <br />
+        <NuxtLink prefetched-class="bg-yellow" to="/">NuxtLink Home</NuxtLink>
+        <NuxtLink to="https://youtube.com/@gymcoding">NuxtLink youtube</NuxtLink>
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script setup lang="ts"></script>
---
+++ pages/admin/dynamic-layout.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="q-pa-md">
+    <div class="q-my-xl text-center q-gutter-md">
+      <q-btn label="default layout" color="dark" @click="changePageLayuout('default')" />
+      <q-btn label="admin layout" color="dark" @click="changePageLayuout('admin')" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { LayoutKey } from '#build/types/layouts';
+const changePageLayuout = (layout: LayoutKey) => {
+  setPageLayout(layout);
+};
+
+definePageMeta({
+  layout: false,
+});
+</script>
+
+<style scoped></style>
